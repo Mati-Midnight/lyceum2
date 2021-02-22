2bdd920ced8c62fb71c62f9c071e5e2af156f6cb
matia <matia.kotik@yandex.ru>
2021-02-23 02:24:14 +0300

    random size and color circle

--- main.py
+++ main.py
@@ -2,16 +2,44 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import QPainter, QPen, QColor
 from PyQt5.QtCore import Qt
 from random import randint
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(500, 500)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(170, 370, 150, 61))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Git и желтые окружности"))
+        self.pushButton.setText(_translate("MainWindow", "старт"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.do_paint = False
-        uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -26,7 +54,8 @@ class MyWidget(QMainWindow):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setPen(QPen(Qt.yellow,  3, Qt.SolidLine))
+        qp.setPen(QPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)),
+                       3, Qt.SolidLine))
         r = randint(10, 300)
         qp.drawEllipse(250 - r // 2, 200 - r // 2, r, r)
 
